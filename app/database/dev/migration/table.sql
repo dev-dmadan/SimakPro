10de65f631ea1c11b75d066dd0b3e071b46b60b9
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-05-03 23:30:59 +0700

    Update table.sql

--- app/database/dev/migration/table.sql
+++ app/database/dev/migration/table.sql
@@ -1,9 +1,9 @@
 # Local Development Only
 
 -- Remove commentary if you want build database from zero
-# DROP DATABASE IF EXISTS `dev-hello-framework`;
-# CREATE DATABASE `dev-hello-framework`;
-# USE `dev-hello-framework`;
+# DROP DATABASE IF EXISTS `dev-simakpro`;
+# CREATE DATABASE `dev-simakpro`;
+# USE `dev-simakpro`;
 
 # End Local Development Only
 
@@ -125,3 +125,46 @@ CREATE TABLE IF NOT EXISTS access_right (
     CONSTRAINT fk_access_right_userId FOREIGN KEY (userId) REFERENCES user (id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_access_right_accessId FOREIGN KEY (accessId) REFERENCES access (id) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB;
+
+
+# Custom Table
+
+-- Table Bank
+DROP TABLE IF EXISTS bank;
+CREATE TABLE IF NOT EXISTS bank (
+    id CHAR(36) NOT NULL,
+    name VARCHAR(250) DEFAULT NULL,
+    saldo DOUBLE(14,2) DEFAULT 0,
+    active_statusId CHAR(36) NOT NULL,
+    created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by CHAR(36) DEFAULT NULL,
+    modified_by CHAR(36) DEFAULT NULL,
+
+    CONSTRAINT pk_bank_id PRIMARY KEY (id),
+    CONSTRAINT fk_bank_active_statusId FOREIGN KEY (active_statusId) REFERENCES active_status (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_bank_created_by FOREIGN KEY (created_by) REFERENCES contact (id) ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT fk_bank_modified_by FOREIGN KEY (modified_by) REFERENCES contact (id) ON DELETE SET NULL ON UPDATE CASCADE
+)ENGINE=InnoDB;
+
+-- Table Mutasi Bank
+DROP TABLE IF EXISTS mutasi_bank;
+CREATE TABLE IF NOT EXISTS mutasi_bank (
+    id CHAR(36) NOT NULL,
+    bankId CHAR(36) NOT NULL,
+    date DATE DEFAULT NULL,
+    credit DOUBLE(14,2) DEFAULT 0,
+    debt DOUBLE(14,2) DEFAULT 0,
+    saldo DOUBLE(14,2) DEFAULT 0,
+    created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by CHAR(36) DEFAULT NULL,
+    modified_by CHAR(36) DEFAULT NULL,
+
+    CONSTRAINT pk_mutasi_bank_id PRIMARY KEY (id),
+    CONSTRAINT fk_mutasi_bank_bankId FOREIGN KEY (bankId) REFERENCES bank (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_mutasi_bank_created_by FOREIGN KEY (created_by) REFERENCES contact (id) ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT fk_mutasi_bank_modified_by FOREIGN KEY (modified_by) REFERENCES contact (id) ON DELETE SET NULL ON UPDATE CASCADE
+)ENGINE=InnoDB;
+
+# End Custom Table
