cc2212f2dd792ec24d204e64944c1ba1a65f326d
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-09-10 16:00:21 +0700

    Create alert.js
    Alert Helpers
    - Swal (sweetalert2)
    - Toastr (sweetalert2)
    - Confirm Swal (sweetalert2)

---
+++ resources/js/helpers/alert.js
@@ -0,0 +1,129 @@
+import Swal from 'sweetalert2';
+export class AlertHelper {
+    constructor() {
+        
+    }
+
+    /** Alert Type */
+        static get Success() {
+            return 'success';
+        }
+        
+        static get Error() {
+            return 'error';
+        }
+
+        static get Warning() {
+            return 'warning';
+        }
+
+        static get Info() {
+            return 'info';
+        }
+
+        static get Question() {
+            return 'question';
+        }
+    /** End Alert Type */
+
+    /** Toastr Position */
+        static get Top() {
+            return 'top';
+        }
+
+        static get TopStart() {
+            return 'top-start';
+        }
+
+        static get TopEnd() {
+            return 'top-end';
+        }
+
+        static get Center() {
+            return 'center'
+        }
+
+        static get CenterStart() {
+            return 'center-start';
+        }
+
+        static get Bottom() {
+            return 'bottom';
+        }
+
+        static get BottomStart() {
+            return 'bottom-start';
+        }
+
+        static get BottomEnd() {
+            return 'bottom-end';
+        }
+    /** End Toastr Position */
+
+    static Alert({
+        title = null,
+        message,
+        type = AlertHelper.Success
+    } = {}) {
+        let message_ = '';
+        if(arguments.length > 0 && typeof arguments[0] === 'string') {
+            message_ = arguments[0];
+        } else if(arguments.length > 0 && typeof arguments[0] === 'object') {
+            message_ = message;
+        }
+
+        Swal.fire({
+            icon: type,
+            title: title != undefined ? title : 'Information',
+            text: message_
+        });
+    }
+
+    static Toastr({
+        message,
+        type = AlertHelper.Success,
+        position = AlertHelper.TopEnd
+    } = {}) {
+        let message_ = '';
+        if(arguments.length > 0 && typeof arguments[0] === 'string') {
+            message_ = arguments[0];
+        } else if(arguments.length > 0 && typeof arguments[0] === 'object') {
+            message_ = message;
+        }
+
+        const Toast = Swal.mixin({
+            toast: true,
+            position: position,
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            onOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        });
+
+        Toast.fire({
+            icon: type,
+            title: message_
+        })
+    }
+
+    static async Confirm({
+        title = null,
+        message = null,
+        type = AlertHelper.Question
+    } = {}) {
+        const confirmALert = await Swal.fire({
+            title: title != undefined ? title : 'Are you sure?',
+            text: message != undefined ? message : '',
+            icon: type,
+            showCancelButton: true,
+            confirmButtonColor: '#3490dc',
+            cancelButtonColor: '#e3342f',
+            confirmButtonText: 'Yes'
+        });
+    
+        return confirmALert.value != undefined ? confirmALert.value : false;
+    };
+}
