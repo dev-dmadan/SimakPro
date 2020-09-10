6511e072cb11c3f8a206a06bcedf7e3167e39dd4
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-09-10 16:04:38 +0700

    Init Resources JS, CSS, NPM, Composer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+import { AlertHelper } from './helpers/alert';
+
+/** Init Global Function */
+    window.Alert = (args) => AlertHelper.Alert(args);
+    window.Toastr = (args) => AlertHelper.Toastr(args);
+    window.Confirm = (args) => AlertHelper.Confirm(args);
+/** End Init Global Function */
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,14 +1,34 @@
 window._ = require('lodash');
 
 /**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    // require('bootstrap');
+    require('../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle');
+} catch (e) {}
+
+/** Default Plugins */
+    require('overlayscrollbars');
+/** End Default Plugins */
+
+require('../../node_modules/admin-lte/dist/js/adminlte');
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+// window.axios = require('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
