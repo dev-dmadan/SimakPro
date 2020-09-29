2479f465eaca72d72f06ccd8604c357921c0ee60
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-09-29 08:59:01 +0700

    register library cleave js dan datepicker
    - register Cleave js
    - register datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,16 @@
 require('./bootstrap');
 
+// import { HTTPClient } from './libraries/httpClient/httpClient';
 import { AlertHelper } from './libraries/alert/alert';
 import { LoadingHelper } from './libraries/loading/loading';
 import { init as CollapseDetail } from './libraries/collapseDetail';
+import { Datepicker } from 'vanillajs-datepicker';
+import Cleave from 'cleave.js';
+
+// window.HTTPClient = HTTPClient;
+window.AlertHelper = AlertHelper;
+window.Datepicker = Datepicker;
+window.Cleave = Cleave;
 
 /** Init Global Function */
     window.Alert = (args) => AlertHelper.Alert(args);
