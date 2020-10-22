5b8cabd5c2c465fb7e052d1df2e42aecabae3a36
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-10-22 09:33:43 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,15 @@ require('./bootstrap');
 
 import { LoadingHelper } from './libraries/loading/loading';
 import { init as CollapseDetail } from './libraries/collapseDetail';
+import { AlertHelper } from './libraries/alert/alert';
 
 /** Init Global Function */
     window.Loading = (args) => LoadingHelper.showLoading(args);
     window.Shimmer = (args) => LoadingHelper.showShimmer(args);
+
+    window.Alert = (args) => AlertHelper.Alert(args);
+    window.Toastr = (args) => AlertHelper.Toastr(args);
+    window.Confirm = (args) => AlertHelper.Confirm(args);
 /** End Init Global Function */
 
 window.ADD_MODE = 'Add';
